122be55c02ea9715a6dbade930306f87c14db95f
Genco Levi <genco.levi@gmail.com>
2020-08-31 18:10:47 +0300

    changed atolye color back

--- src/Launcher.vue
+++ src/Launcher.vue
@@ -11,7 +11,7 @@
         v-if="newMessagesCount > 0 && !isOpen"
         class="sc-new-messsages-count"
       >{{ newMessagesCount }}</div>
-      <img v-if="isOpen" class="sc-closed-icon" :src="icons.close.img" :alt="icons.close.name" :style="{backgroundColor: '#D82028'}" />
+      <img v-if="isOpen" class="sc-closed-icon" :src="icons.close.img" :alt="icons.close.name" />
       <img v-else class="sc-open-icon" :src="icons.open.img" :alt="icons.open.name" />
     </div>
     <ChatWindow
